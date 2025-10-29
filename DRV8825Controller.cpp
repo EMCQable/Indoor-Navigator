b968c447cf29c6691f149a292090370222451a17
Timo Polvinen <timo.polvinen@gmail.com>
2025-10-30 01:06:41 +0200

    simplified setting the resolution

--- DRV8825Controller.cpp
+++ DRV8825Controller.cpp
@@ -1,164 +1,68 @@
 #include "DRV8825Controller.hpp"
+
 #include <Arduino.h>
+
 #include <cassert>
 
-DRV8825Controller::DRV8825Controller( 
-        int stepResolution,
-        int m1pin,
-        int m2pin,
-        int m3pin,
-        int resetpin,
-        int sleeppin,
-        int steppin,
-        int enablepin,
-        int dirpin) : 
-        stepsPerRevolution_(10),
-        m1Pin_(m1pin),
-        mPin_(m2pin),
-        m3Pin_(m3pin),
-        resetPin_(resetpin),
-        sleepPin_(sleeppin),
-        stepPin_(steppin),
-        dirPin_(dirpin),
-        enablePin_(enablepin) {
-        // set output pins
-        if (m1Pin_ > 0) {
-            pinMode(m1Pin_, OUTPUT);
-        }
+DRV8825Controller::DRV8825Controller(char stepResolution, char resolutionPin1, char resolutionPin2,
+                                     char resolutionPin3, char resetpin, char sleeppin, char steppin,
+                                     char enablepin, char dirpin)
+    : stepsPerRevolution_(10),
+      resolutionPin1_(resolutionPin1),
+      resolutionPin2_(resolutionPin2),
+      resolutionPin3_(resolutionPin3),
+      resetPin_(resetpin),
+      sleepPin_(sleeppin),
+      stepPin_(steppin),
+      dirPin_(dirpin),
+      enablePin_(enablepin) {
+    // set output pins
+    if (m1Pin_ > 0) {
+        pinMode(m1Pin_, OUTPUT);
+    }
 
-        if (mPin_ > 0) {
-            pinMode(mPin_, OUTPUT);
-        }
+    if (mPin_ > 0) {
+        pinMode(mPin_, OUTPUT);
+    }
 
-        if (m3Pin_ > 0) {
-            pinMode(m3Pin_, OUTPUT);
-        }
+    if (m3Pin > 0) {
+        pinMode(m3Pin, OUTPUT);
+    }
 
-        if (resetPin_ > 0) {
-            pinMode(resetPin_, OUTPUT);
-        }
+    if (resetPin_ > 0) {
+        pinMode(resetPin_, OUTPUT);
+    }
 
-        if (sleepPin_ > 0) {
-            pinMode(sleepPin_, OUTPUT);
-        }
+    if (sleepPin_ > 0) {
+        pinMode(sleepPin_, OUTPUT);
+    }
 
-        assert(stepPin_ > 0);
-        assert(dirPin_ > 0);
+    assert(stepPin_ > 0);
+    assert(dirPin_ > 0);
 
-        pinMode(stepPin_, OUTPUT);
-        pinMode(dirPin_, OUTPUT);
-        // set the correct rate
-        bool ret = setStepResolution(stepResolution);
+    pinMode(stepPin_, OUTPUT);
+    pinMode(dirPin_, OUTPUT);
+    // set the correct rate
+    bool ret = setStepResolution(stepResolution);
 }
 
-bool DRV8825Controller::setStepResolution(int stepResolution) {
-        switch (stepResolution)
-        {
-            case 1:
-                if (mPin_ > 0){
-                    digitalWrite(mPin_,LOW);
-                }
-                if (m3Pin_ > 0) {
-                    digitalWrite(m3Pin_,LOW);
-                }
-                if (m1Pin_ > 0){
-                    digitalWrite(m1Pin_,LOW);
-                }
-                return true;
-                break;
-            case 2:
-                if (m1Pin_ > 0){
-                    digitalWrite(m1Pin_,HIGH);
-                } else {
-                    return false;
-                }
-                if (mPin_ > 0){
-                    digitalWrite(mPin_,LOW);
-                }
-                if (m3Pin_ > 0) {
-                    digitalWrite(m3Pin_,LOW);
-                }
-                return true;
-                break;
-            case 4:
-                if (m1Pin_ > 0) {
-                    digitalWrite(m1Pin_,LOW);
-                }
-                if (mPin_ > 0) {
-                    digitalWrite(mPin_,HIGH);
-                } else {
-                    return false;
-                }
-                if (m3Pin_ > 0) {
-                    digitalWrite(m3Pin_,LOW);
-                }
-                return true;
-                break;
-            case 8:
-                if (m1Pin_ > 0) {
-                    digitalWrite(m1Pin_,HIGH);
-                } else{
-                    return false;
-                }
-                if (mPin_ > 0) {
-                    digitalWrite(mPin_,HIGH);
-                } else {
-                    return false;
-                }
-                if (m3Pin_ > 0) {
-                    digitalWrite(m3Pin_,LOW);
-                }
-                return true;
-                break;
-            case 16:
-                if (m1Pin_ > 0) {
-                    digitalWrite(m1Pin_,LOW);
-                }
-                if (mPin_ > 0) {
-                    digitalWrite(mPin_,LOW);
-                } else {
-                    return false;
-                }
-                if (m3Pin_ > 0) {
-                    digitalWrite(m3Pin_,HIGH);
-                } else {
-                    return false;
-                }
-                return true;
-                break;
-            case 32:
-                // TODO: there could be better if loop to go through all possibilities
-                // for example, M1 HIGH, M2 HIGH and M3 HIGH
-                if (m1Pin_ > 0) {
-                    digitalWrite(m1Pin_,HIGH);
-                } else {
-                    return false;
-                }
-                if (mPin_ > 0) {
-                    digitalWrite(mPin_,LOW);
-                } 
-                if (m3Pin_ > 0) {
-                    digitalWrite(m3Pin_,HIGH);
-                } else {
-                    return false;
-                }
-                return true;
-                break;
-            default:
-                // keep same
-                break;
-        }
-        return false;
+// does log2 on the resolution and then sets the pins high or low depending on bits
+bool DRV8825Controller::setStepResolution(char stepResolution) {
+    int pins = 0 = sizeof(int) - std::countl_zero(stepResolution) - 1;
+    digitalWrite(resolutionPin1_, pins >> 1);
+    digitalWrite(resolutionPin1_, pins >> 2);
+    digitalWrite(resolutionPin1_, pins >> 3);
+    return true;
 }
 
 bool DRV8825Controller::togglePin(int pin_num) {
-    if (pin_num > 0 ) {
-        // TODO: double check that this is okay? 
+    if (pin_num > 0) {
+        // TODO: double check that this is okay?
         int state = digitalRead(pin_num);
         if (state == LOW) {
-            digitalWrite(pin_num,HIGH);
+            digitalWrite(pin_num, HIGH);
         } else {
-            digitalWrite(pin_num,LOW);
+            digitalWrite(pin_num, LOW);
         }
         return true;
     } else {
@@ -166,32 +70,26 @@ bool DRV8825Controller::togglePin(int pin_num) {
     }
 }
 
-bool DRV8825Controller::toggleReset() {
-    return togglePin(resetPin_);
-}
+bool DRV8825Controller::toggleReset() { return togglePin(resetPin_); }
 
-bool DRV8825Controller::toggleSleep() {
-    return togglePin(sleepPin_);
-}
+bool DRV8825Controller::toggleSleep() { return togglePin(sleepPin_); }
 
-bool DRV8825Controller::toggleEnable() {
-    return togglePin(enablePin_);
-}
+bool DRV8825Controller::toggleEnable() { return togglePin(enablePin_); }
 
 // if direction is true or 1 or HIGH, clockwise direction
 // if direction is false or 0 or LOW, counter clockwise direction
 void DRV8825Controller::takeStep(bool direction) {
     if (direction) {
-        digitalWrite(dirPin_,HIGH);
+        digitalWrite(dirPin_, HIGH);
     } else {
-        digitalWrite(dirPin_,LOW);
+        digitalWrite(dirPin_, LOW);
     }
     // TODO: simple for loop taken from tutorial
     // needs to be better
-    for (int x = 0; x < stepsPerRevolution_; x++) {
-        digitalWrite(stepPin_,HIGH);
+    for (int step = 0; step < stepsPerRevolution_; step++) {
+        digitalWrite(stepPin_, HIGH);
         delay(20);
-        digitalWrite(stepPin_,LOW);
+        digitalWrite(stepPin_, LOW);
     }
 }
 
--- DRV8825Controller.hpp
+++ DRV8825Controller.hpp
@@ -3,10 +3,10 @@ class DRV8825Controller {
     * A class controlling DRV8825 Controller Circuit 
     */
     public:
-        DRV8825Controller(int turnratedenominator, int m1pin, int m2pin, int m3pin, 
+        DRV8825Controller(int turnratedenominator, int resPin1, int resPin2, int resPin3, 
             int resetpin, int sleeppin, int steppin, int enablepin, int dirpin);
         bool setStepsPerRevolution(int new_StepsPerRevolution);
-        bool setStepMode(int denominator);
+        bool setStepResolution(char resolution);
         bool toggleReset();
         bool toggleSleep();
         bool toggleEnable();
@@ -17,16 +17,16 @@ class DRV8825Controller {
         
         // TODO: the memory consumption could be cut by choosing
         // smaller types than int
-        int stepsPerRevolution_;
+        int stepResolution_;
         int turnRate_;
 
         // Pins
-        const int stepPin_;
-        const int dirPin_;
-        const int m1Pin_;
-        const int mPin_;
-        const int m3Pin_;
-        const int enablePin_;
-        const int resetPin_;
-        const int sleepPin_;
+        const char stepPin_;
+        const char dirPin_;
+        const unsigned char resolutionPin1_;
+        const unsigned char resolutionPin2_;
+        const unsigned char resolutionPin3_;
+        const char enablePin_;
+        const char resetPin_;
+        const char sleepPin_;
 };
